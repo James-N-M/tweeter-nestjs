0092bb9f94d1db8a0d59120f940bd670290fc10a
James Moore <jamesmoore@rocketinnovationstudio.ca>
2022-01-21 15:06:42 -0500

    JWT authentication strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private authService: AuthService) {}
 
   // DONT FORGET ABOUT CONTROLLER ROUTE NAMES /controllername/thing
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  login(): any {
-    return {
-      'access_token': "thisisarandomaccesstokenplaceholder1234556789"
-    };
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  // JWT guarded route
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 
   @Get('/products')
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'secretKey', // TODO move into env variable
+  };
