b879b8edf20b323575b99e2aadb37d3a2e9f1b8e
James Moore <jamesmoore@rocketinnovationstudio.ca>
2022-02-12 17:57:27 -0500

    A User can Follow another user

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import RequestWithUser from 'src/auth/requestWithUser.interface';
 import { ProfilesService } from './profiles.service';
 
@@ -8,11 +9,16 @@ import { ProfilesService } from './profiles.service';
 export class ProfilesController {
  constructor(private readonly profilesService: ProfilesService) {}
 
- @ApiOperation({ summary: 'Get profile' })
- @ApiResponse({ status: 200, description: 'Return a users profile.' })
- @Get(':username')
- async getProfile(@Req() req: RequestWithUser, @Param('username') username: string) {
-
+  @ApiOperation({ summary: 'Get profile' })
+  @ApiResponse({ status: 200, description: 'Return a users profile.' })
+  @Get(':username')
+  async getProfile(@Req() req: RequestWithUser, @Param('username') username: string) {
     return await this.profilesService.findProfile(null, username);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(JwtAuthGuard)
+  async follow(@Req() req: RequestWithUser, @Param('username') username: string) {
+    return await this.profilesService.follow(req.user, username); 
   }
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import User from 'src/users/user.entity';
 import { Repository } from 'typeorm';
@@ -8,31 +8,68 @@ import { Profile } from './profile.interface';
 @Injectable()
 export class ProfilesService {
 
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
-        @InjectRepository(Follows)
-        private readonly followsRepository: Repository<Follows>
-    ) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Follows)
+    private readonly followsRepository: Repository<Follows>
+  ) { }
 
-    async findProfile(currentUserId: number, followingUsername: string) {
-        let user = await this.userRepository.findOne( {username: followingUsername});
+  async findProfile(currentUserId: number, followingUsername: string) {
+    let user = await this.userRepository.findOne({ username: followingUsername });
+
+    if (!user) return;
+
+    let profile: Profile = {
+      username: user.username,
+      bio: user.bio,
+    };
+
+    const follows = await this.followsRepository.findOne({ followerId: currentUserId, followingId: user.id });
+
+    // 1 call return all records where the user id exists as a follwerID or followingId
+    const _follows = await this.followsRepository.find( {followerId: user.id });
+
+    // const qb = await getRepository(ArticleEntity)
+    // .createQueryBuilder('article')
+    // .where('article.authorId IN (:ids)', { ids });
+    // get follweres ids
+    // get following ids 
+
+    // query both and group by follower ids and following ids.. 
+
+    // two separate queries 
     
-        if(!user) return;
-    
-        let profile: Profile = {
-          username: user.username,
-          bio: user.bio,
-        //   image: _profile.image
-        };
-    
-        const follows = await this.followsRepository.findOne( {followerId: currentUserId, followingId: user.id});
-    
-        if (currentUserId) {
-          profile.following = !!follows;
-        }
-    
-        return profile;
-      }
+    if (currentUserId) {
+      profile.following = !!follows;
+    }
+
+    return profile;
+  }
+
+  async follow(currentUser, username: string) {
+    const followingUser = await this.userRepository.findOne({username});
+
+    if (currentUser.email === followingUser.email) {
+      throw new HttpException('Current User and Following User cannot be equal. ', HttpStatus.BAD_REQUEST);
+    }
+
+    const _follows = await this.followsRepository.findOne({ followerId: currentUser.id, followingId: followingUser.id});
+
+    if(!_follows) {
+      const follows = new Follows();
+      follows.followerId = currentUser.id,
+      follows.followingId = followingUser.id;
+      await this.followsRepository.save(follows);
+    }
+
+    let profile: Profile = {
+      username: followingUser.username,
+      bio: followingUser.bio,
+      following: true
+    }
+
+    return profile;
+  }
 }
 
